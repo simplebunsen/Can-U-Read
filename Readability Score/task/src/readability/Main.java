366982eb25d12ef8195883f48f519c4c6e30051e
simplebunsen <really@dontdoxme.com>
2020-05-01 17:22:03 +0200

    Stage 3: We read a file now and can calculate an age corresponding to the readability score.

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -1,16 +1,56 @@
 package readability;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String text = scanner.nextLine();
+    public static void main(String[] args) throws IOException {
+        String fileInput = args[0];
+        String text = new String(Files.readAllBytes(Paths.get(fileInput)));
 
-        if (text.length() > 100) {
-            System.out.println("HARD");
-        } else {
-            System.out.println("EASY");
+        System.out.println("The text is:\n" + text);
+        System.out.println();
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        String text = scanner.nextLine();
+
+        String[] sentences = text.split("[.?!]");
+
+        int sentenceCounter = 0;
+        int wordSum = 0;
+        for (String sentence : sentences) {
+            sentenceCounter++;
+//            sentence = sentence.replaceAll("[^A-Za-z0-9\\s]", "");
+//            System.out.println(sentence);
+            String[] words = sentence.split(" ");
+            List<String> temp = new ArrayList<>(Arrays.asList(words));
+            temp.removeAll(Arrays.asList(""));
+            wordSum += temp.size();
         }
+
+
+        String textOnlyChars = text.replaceAll("\\s", "");
+
+        double score = 4.71 * ((double) textOnlyChars.length() / wordSum) + 0.5 * ((double) wordSum / sentenceCounter) - 21.43;
+
+
+        System.out.printf("Words: %d\n" +
+                "Sentences: %d\n" +
+                "Characters: %d\n" +
+                "The score is: %.2f\n", wordSum, sentenceCounter, textOnlyChars.length(), score);
+
+        int scoreInt = (int) Math.ceil(score);
+        //System.out.println("ROUNDED SCORE: " + scoreInt);
+
+        final String[] steps = {"5-6", "6-7", "7-9", "9-10", "10-11", "11-12", "12-13", "13-14", "14-15", "15-16",
+                "16-17", "17-18", "18-24", "24+"};
+
+        System.out.printf("This text should be understood by %s year olds.", steps[scoreInt-1]);
     }
 }
